760dc6c5781af6a1743025e399a770f33c0fa30c
Sorin Veres <sorin.veres@softvision.com>
2022-01-04 15:24:39 +0200

    Updated test cases.

--- PlaywrightSharp/UnitTest1.cs
+++ PlaywrightSharp/UnitTest1.cs
@@ -12,68 +12,131 @@ namespace PlaywrightSharp
         {
         }
 
+        private string _browserConfig;
+        public TestContext TestContext { get; set; }
+
         [Test]
         public async Task Test1()
         {
+            using var playwright = await Playwright.CreateAsync();
+            string _browserConfig = TestContext.Parameters.Get("browser");
+            if (_browserConfig.Contains("Chrome"))
+            {
+                await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+                {
+                    Headless = false,
+                });
+                var context = await browser.NewContextAsync(new BrowserNewContextOptions
+                {
+                    RecordVideoDir = "videos/",
+                    RecordVideoSize = new RecordVideoSize() { Width = 640, Height = 480 }
+                });
+                // Open new page
+
+
+                await context.Tracing.StartAsync(new TracingStartOptions
+                {
+                    Screenshots = true,
+                    Snapshots = true
+                });
+
+
+                var page = await context.NewPageAsync();
+                // Go to http://datingapp-pipeline.azurewebsites.net/
+                await page.GotoAsync("http://datingapp-pipeline.azurewebsites.net/");
+                // Click [placeholder="Username"]
+                await page.ClickAsync("[placeholder=\"Username\"]");
+                // Fill [placeholder="Username"]
+                await page.FillAsync("[placeholder=\"Username\"]", "vinnie");
+                // Press Tab
+                await page.PressAsync("[placeholder=\"Username\"]", "Tab");
+                // Fill [placeholder="Password"]
+                await page.FillAsync("[placeholder=\"Password\"]", "password");
+                // Click text=Login
+                await page.RunAndWaitForNavigationAsync(async () =>
+                {
+                    await page.ClickAsync("text=Login");
+                }/*, new PageWaitForNavigationOptions
+        {
+            UrlString = "http://datingapp-pipeline.azurewebsites.net/members"
+        }*/);
+                // Click text=Welcome Vinnie
+                await page.ClickAsync("text=Welcome Vinnie");
+                // Click text=Logout
+                await page.ClickAsync("text=Logout");
+                // Assert.AreEqual("http://datingapp-pipeline.azurewebsites.net/", page.Url);
+
+                await context.Tracing.StopAsync(new TracingStopOptions
+                {
+                    Path = "trace.zip"
+                });
+
+                await context.CloseAsync();
+            }
+
+            else if (_browserConfig.Contains("Firefox"))
+            {
+                await using var browser = await playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions
+                {
+                    Headless = false,
+                });
+                var context = await browser.NewContextAsync(new BrowserNewContextOptions
+                {
+                    RecordVideoDir = "videos/",
+                    RecordVideoSize = new RecordVideoSize() { Width = 640, Height = 480 }
+                });
+                // Open new page
+
+
+                await context.Tracing.StartAsync(new TracingStartOptions
+                {
+                    Screenshots = true,
+                    Snapshots = true
+                });
+
+
+                var page = await context.NewPageAsync();
+                // Go to http://datingapp-pipeline.azurewebsites.net/
+                await page.GotoAsync("http://datingapp-pipeline.azurewebsites.net/");
+                // Click [placeholder="Username"]
+                await page.ClickAsync("[placeholder=\"Username\"]");
+                // Fill [placeholder="Username"]
+                await page.FillAsync("[placeholder=\"Username\"]", "vinnie");
+                // Press Tab
+                await page.PressAsync("[placeholder=\"Username\"]", "Tab");
+                // Fill [placeholder="Password"]
+                await page.FillAsync("[placeholder=\"Password\"]", "password");
+                // Click text=Login
+                await page.RunAndWaitForNavigationAsync(async () =>
+                {
+                    await page.ClickAsync("text=Login");
+                }/*, new PageWaitForNavigationOptions
+        {
+            UrlString = "http://datingapp-pipeline.azurewebsites.net/members"
+        }*/);
+                // Click text=Welcome Vinnie
+                await page.ClickAsync("text=Welcome Vinnie");
+                // Click text=Logout
+                await page.ClickAsync("text=Logout");
+                // Assert.AreEqual("http://datingapp-pipeline.azurewebsites.net/", page.Url);
+
+                await context.Tracing.StopAsync(new TracingStopOptions
+                {
+                    Path = "trace.zip"
+                });
+
+                await context.CloseAsync();
+            }
             //using var playwright = await Playwright.CreateAsync();
             //await using var browser = await playwright.Chromium.LaunchAsync(new() { Headless = false });
             //var page = await browser.NewPageAsync();
             //await page.GotoAsync("https://playwright.dev/dotnet");
             //await page.ScreenshotAsync(new() { Path = "screenshot.png" });
 
-            using var playwright = await Playwright.CreateAsync();
-            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-            {
-                Headless = false,
-            });
-            var context = await browser.NewContextAsync(new BrowserNewContextOptions
-            {
-                RecordVideoDir = "videos/",
-                RecordVideoSize = new RecordVideoSize() { Width = 640, Height = 480 }
-            });
-            // Open new page
 
 
-            await context.Tracing.StartAsync(new TracingStartOptions
-            {
-                Screenshots = true,
-                Snapshots = true
-            });
 
 
-            var page = await context.NewPageAsync();
-            // Go to http://datingapp-pipeline.azurewebsites.net/
-            await page.GotoAsync("http://datingapp-pipeline.azurewebsites.net/");
-            // Click [placeholder="Username"]
-            await page.ClickAsync("[placeholder=\"Username\"]");
-            // Fill [placeholder="Username"]
-            await page.FillAsync("[placeholder=\"Username\"]", "vinnie");
-            // Press Tab
-            await page.PressAsync("[placeholder=\"Username\"]", "Tab");
-            // Fill [placeholder="Password"]
-            await page.FillAsync("[placeholder=\"Password\"]", "password");
-            // Click text=Login
-            await page.RunAndWaitForNavigationAsync(async () =>
-            {
-                await page.ClickAsync("text=Login");
-            }/*, new PageWaitForNavigationOptions
-        {
-            UrlString = "http://datingapp-pipeline.azurewebsites.net/members"
-        }*/);
-            // Click text=Welcome Vinnie
-            await page.ClickAsync("text=Welcome Vinnie");
-            // Click text=Logout
-            await page.ClickAsync("text=Logout");
-            // Assert.AreEqual("http://datingapp-pipeline.azurewebsites.net/", page.Url);
-
-            await context.Tracing.StopAsync(new TracingStopOptions
-            {
-                Path = "trace.zip"
-            });
-
-            await context.CloseAsync();
-
-            
 
         }
 
